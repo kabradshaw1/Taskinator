59a0e1abdbb2fa5a6fc067964a469d120b116146
kabradshaw1 <itllbok82@gmail.com>
2022-08-18 14:11:44 -0400

    4.2.5

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,22 @@
-var buttonEl = document.querySelector("#save-task");
+
+// formE1 creates the object for the addEventListener
+// at the bottom linking it to the form at the top of the html page
+var formEl = document.querySelector("#task-form");
+// tasksToDoE1 creates the object for the list, to be called, so the new 
+// items can be appended
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() { 
+  event.preventDefault();
+  // creates the task line item, gives the class it needs to get the 
+  // css, for now, it fills in a default text, but should soon fill in the
+  // text you enter, and appends the line item to the list.
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
   listItemEl.textContent = "This is a new task."; 
   tasksToDoEl.appendChild(listItemEl); 
   }; 
-
-buttonEl.addEventListener("click", createTaskHandler);
-  
+// This will run the function createTaskHandler when you click 
+// on the add task or hit enter in the enter while in the
+// enter task field
+formEl.addEventListener("submit", createTaskHandler);
