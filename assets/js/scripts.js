df844550e7889863d16dc60269dc182795ddcd6e
kabradshaw1 <itllbok82@gmail.com>
2022-08-18 16:20:12 -0400

    4.2.6

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,15 +7,36 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() { 
+  // preventDefault was used to turn off the refresh page
+  // when this function runs
   event.preventDefault();
-  // creates the task line item, gives the class it needs to get the 
-  // css, for now, it fills in a default text, but should soon fill in the
-  // text you enter, and appends the line item to the list.
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+  // stores in information typed in the input html element with the name
+  // of task=name.  We found that the input was stored in value property,
+  // by doing a console.dir("taskNameInput")
+  var taskTypeInput = document.querySelector("select[name='task-type']")
+    .value;
+  var taskNameInput = document.querySelector("input[name='task-name']")
+    .value;
+  
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + 
+    "</h3><span class='task-type'>" + taskTypeInput + 
+    "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+  console.dir(listItemEl);
+}; 
 // This will run the function createTaskHandler when you click 
 // on the add task or hit enter in the enter while in the
 // enter task field
